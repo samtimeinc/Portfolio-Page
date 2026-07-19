309f2a065b215bd55d5d9fd4cb28984030a31a0e
samtimeinc <samtime.worldwide@gmail.com>
2026-07-18 17:31:16 -0700

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Sam Phin Portfolio - Welcome!',
-  description: 'Portfolio website for Sam Phin, a frontend software engineer specializing Typescript, React, Next.js, and Node.js. Explore my projects, skills, and experience in web development.',
+  title: 'Sam Phin - Welcome to my portfolio!',
+  description: 'Sam Phin is a frontend software engineer specializing in TypeScript, React, Next.js, and Node.js. Explore my projects, skills, and experience in web development. Take the lessons from yesterday, apply them today, and create a better tomorrow. - Sam Phin',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
